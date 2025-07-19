2c1bae7a01f43e5d882c6ef9120484863f371215
Narfware <narfware@gmail.com>
2025-07-19 19:46:40 +0200

    create Time ValueObject and their provider

---
+++ src/enigma/domain/time.ts
@@ -0,0 +1,23 @@
+export class Time {
+    private constructor(private readonly date: Date) {}
+
+    public equals(time: Time): boolean {
+        return this.toPrimitives() === time.toPrimitives()
+    }
+
+    public static now(): Time {
+        return new Time(new Date())
+    }
+
+    public toPrimitives(): number {
+        return this.date.getTime()
+    }
+
+    public static fromPrimitives(timestamp: number): Time {
+        return new Time(new Date(timestamp))
+    }
+}
+
+export interface TimeProvider {
+    now(): Time
+}
