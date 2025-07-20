623e60752456acbec44cb37dc57f1695cf68d0e0
Narfware <narfware@gmail.com>
2025-07-20 20:23:33 +0200

    green test for happy path

--- src/enigma/infrastructure/nodeCryptoSignatureVerifier.ts
+++ src/enigma/infrastructure/nodeCryptoSignatureVerifier.ts
@@ -1,6 +1,11 @@
 import { SignatureVerifier } from '../domain/interfaces/signatureVerifier'
 import { Nonce } from '../domain/value-objects/nonce'
+import { verify } from 'crypto'
 
 export class NodeCryptoSignatureVerifier implements SignatureVerifier {
-    checkIsValidSignatureForNonce(nonce: Nonce, publicKey: string, signature: string): { isValidSignature: boolean } {}
+    checkIsValidSignatureForNonce(nonce: Nonce, publicKey: string, signature: string): { isValidSignature: boolean } {
+        const isValidSignature = verify('sha256', Buffer.from(nonce.value), publicKey, Buffer.from(signature, 'base64'))
+
+        return { isValidSignature }
+    }
 }
